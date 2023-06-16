2ca3571371c3e75d90d1bb71851d74399d2f3cca
Marline John <103510248+marlinejohn@users.noreply.github.com>
2023-06-16 18:02:39 +0200

    Removed unnecessary files and included GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          href="https://github.com/marlinejohn/redi-react-weather-app"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Marline John
+      </footer>
+    </div>
+  );
 }
 
 export default App;
