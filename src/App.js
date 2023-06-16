beb5f40fdcf1f16975db10362921d6acf2c2b903
Marline John <103510248+marlinejohn@users.noreply.github.com>
2023-06-16 18:41:47 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a
-          href="https://github.com/marlinejohn/redi-react-weather-app"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Marline John
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          <a
+            href="https://github.com/marlinejohn/redi-react-weather-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Marline John
+        </footer>
+      </div>
     </div>
   );
 }
