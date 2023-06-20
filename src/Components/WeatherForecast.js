97c9bbf0cb549457ee2952070f9199312834dee0
Marline John <103510248+marlinejohn@users.noreply.github.com>
2023-06-20 23:34:28 +0200

    Added and improved WeatherForecast

--- src/Components/WeatherForecast.js
+++ src/Components/WeatherForecast.js
@@ -1,10 +1,19 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 const WeatherForecast = (props) => {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">"Thu" "Time"</div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-max">19º</span>
+            <span className="WeatherForecast-min">10º</span>
+          </div>{" "}
+        </div>
       </div>
     </div>
   );
--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -17,7 +17,7 @@ const WeatherInfo = (props) => {
         <div className="col-6">
           <div className="clearfix ">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={50} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
             <div className="float-left">
